c41219235c329c825494319de1325dae4cace606
Andrés Muñoz O. <andmunoz@gmail.com>
2026-07-15 20:36:57 -0400

    Using IA Agent to modify MainActivity.kt and AndroidManifest.xml

--- Ejemplos/HelloWorld/app/src/main/java/cl/uchile/dcc/mobile/helloworld/ui/MainActivity.kt
+++ Ejemplos/HelloWorld/app/src/main/java/cl/uchile/dcc/mobile/helloworld/ui/MainActivity.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import cl.uchile.dcc.mobile.helloworld.ui.theme.HelloWorldTheme
 
 class MainActivity : ComponentActivity() {
@@ -64,12 +73,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HelloWorld(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    Column(
         modifier = modifier.fillMaxSize(),
-        textAlign = TextAlign.Center,
-        fontFamily = FontFamily.Serif
-    )
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "¡Bienvenido, $name!",
+            textAlign = TextAlign.Center,
+            fontFamily = FontFamily.Serif
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = { /* Acción al hacer clic */ },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+        ) {
+            Text(text = "Presiona AQUI")
+        }
+    }
 }
 
 @Preview(showBackground = true)
